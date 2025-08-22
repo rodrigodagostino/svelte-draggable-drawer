a6e08be3934d0b4296ee66fd9210f9f601acdb9f
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2025-08-21 22:43:32 -0300

    feat: add keyboard support

--- src/lib/components/DraggableDrawerHandle.svelte
+++ src/lib/components/DraggableDrawerHandle.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { getHandle } from '$lib/stores/index.js';
+	import { getHandle, getIsExpanded } from '$lib/stores/index.js';
 
 	let handleRef: HTMLButtonElement;
 
 	const handle = getHandle();
 
+	const isExpanded = getIsExpanded();
+
 	onMount(() => {
 		$handle = handleRef;
 	});
+
+	const handleKeyUp = ({ key }: KeyboardEvent) => {
+		if (key === 'Enter' || key === ' ') {
+			$isExpanded = !$isExpanded;
+		}
+	};
 </script>
 
-<button bind:this={handleRef} class="sdd-handle" data-role="handle">
+<button bind:this={handleRef} class="sdd-handle" data-role="handle" on:keyup={handleKeyUp}>
 	<slot />
 </button>
 
