42fa005876726303b60540e1866ae07927d73bd5
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2026-09-25 21:26:22 -0300

    docs: ensure marker texts are always visible

--- src/routes/components/Markers.svelte
+++ src/routes/components/Markers.svelte
@@ -7,47 +7,75 @@
 		RANGE_START_DEFAULT,
 		SNAP_POINT_OFFSET,
 	} from '$lib/constants/index.js';
+
+	const rangeStart = $derived(toPixels(layoutState.props.range?.start, RANGE_START_DEFAULT));
+	const rangeEnd = $derived(toPixels(layoutState.props.range?.end, RANGE_END_DEFAULT));
+
+	function getMarkerTextTransform(value: number, type: 'snap-point' | 'range-start' | 'range-end') {
+		const markerTextHeight = 26;
+		const offset = markerTextHeight + markerTextHeight / 2;
+		const newValue =
+			value < offset && type !== 'range-end'
+				? `-${markerTextHeight}px`
+				: value < offset && type === 'range-end'
+					? 0
+					: value > window.innerHeight - offset
+						? 0
+						: '-50%';
+		return `translate(-50%, ${newValue})`;
+	}
 </script>
 
 <div class="markers">
 	{#each layoutState.props.snapPoints as snapPoint, index (index)}
+		{@const snapPointInPx = toPixels(snapPoint, 0)}
 		<div
 			class="marker marker--snap-point-offset-top"
-			style:bottom="{toPixels(snapPoint, 0) - SNAP_POINT_OFFSET}px"
+			style:bottom="{snapPointInPx - SNAP_POINT_OFFSET}px"
 			transition:fade|global={{ duration: 200 }}
 		>
 			<span class="marker__text">{SNAP_POINT_OFFSET}</span>
 		</div>
 		<div
 			class="marker marker--snap-point-offset-bottom"
-			style:bottom="{toPixels(snapPoint, 0) + SNAP_POINT_OFFSET}px"
+			style:bottom="{snapPointInPx + SNAP_POINT_OFFSET}px"
 			transition:fade|global={{ duration: 200 }}
 		>
 			<span class="marker__text">{SNAP_POINT_OFFSET}</span>
 		</div>
 	{/each}
 	{#each layoutState.props.snapPoints as snapPoint, index (index)}
+		{@const snapPointInPx = toPixels(snapPoint, 0)}
 		<div
 			class="marker marker--snap-point"
-			style:bottom="{toPixels(snapPoint, 0)}px"
+			style:bottom="{snapPointInPx}px"
 			transition:fade|global={{ duration: 200 }}
 		>
-			<span class="marker__text">snap point {index}: {snapPoint}</span>
+			<span
+				class="marker__text"
+				style:transform={getMarkerTextTransform(snapPointInPx, 'snap-point')}
+			>
+				snap point {index}: {snapPoint}
+			</span>
 		</div>
 	{/each}
 	<div
 		class="marker marker--range-start"
-		style:bottom="{toPixels(layoutState.props.range?.start, RANGE_START_DEFAULT)}px"
+		style:bottom="{rangeStart}px"
 		transition:fade|global={{ duration: 200 }}
 	>
-		<span class="marker__text">range start: {layoutState.props.range?.start}</span>
+		<span class="marker__text" style:transform={getMarkerTextTransform(rangeStart, 'range-start')}>
+			range start: {rangeStart}
+		</span>
 	</div>
 	<div
 		class="marker marker--range-end"
-		style:bottom="calc(100% - {toPixels(layoutState.props.range?.end, RANGE_END_DEFAULT)}px)"
+		style:bottom="calc(100% - {rangeEnd + 2}px)"
 		transition:fade|global={{ duration: 200 }}
 	>
-		<span class="marker__text">range end: {layoutState.props.range?.end}</span>
+		<span class="marker__text" style:transform={getMarkerTextTransform(rangeEnd, 'range-end')}>
+			range end: {rangeEnd}
+		</span>
 	</div>
 </div>
 
@@ -76,6 +104,7 @@
 		transform: translate(-50%, -50%);
 		padding: 0.1875rem 0.25rem 0.125rem;
 		border-radius: 0.125rem;
+		transition: transform 400ms;
 	}
 
 	.marker--range-start,
